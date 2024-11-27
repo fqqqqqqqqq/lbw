ae5e385519f857dbb9254bd518924fef8294e5cf
kaiming feng <fye@kaimingdeMacBook-Air.local>
2024-11-27 14:53:05 +0300

    Your commit message here

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-   document.getElementById('addTaskButton').addEventListener('click', function() {
-       const taskInput = document.getElementById('taskInput');
-       const taskValue = taskInput.value;
 
-       if (taskValue) {
-           const li = document.createElement('li');
-           li.textContent = taskValue;
-           document.getElementById('taskList').appendChild(li);
-           taskInput.value = '';
-       }
-   });
